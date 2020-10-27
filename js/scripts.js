06ffe87dcfc20ba7896c2a937e53c9149484b79a
Pugsly99 <rylandadams@yahoo.com>
2020-10-27 13:15:04 -0700

    examples of the right UI and BL
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,33 @@ $(document).submit(function(){
   for (let i = 0; i <= word.length; i++) {
 
     if(word.charAt(i) === "a" || word.charAt(i) === "e" || word.charAt(i) === "i" || word.charAt(i) === "o" || word.charAt(i) === "u"){
-    //console.log("-");
     $("#output").append("-");
     }
-    else {
-      //console.log(word.charAt(i));
+    else {     
       $("#output").append(word.charAt(i));
     }
     $("#vowel").val("");
   }
 });
+
+
+
+
+
+// Business Logic
+// function vowelFinder(sentence) {
+//   for (let i = 0; i <= sentence.length; i++) {
+//     if(sentence.charAt(i) === "a" || sentence.charAt(i) === "e" || sentence.charAt(i) === "i" || sentence.charAt(i) === "o" || sentence.charAt(i) === "u"){
+//       sentence[i] = "-";
+//     }
+//   }
+//   return sentence;
+// }
+
+// // UI Logic
+// $(document).submit(function(){
+//   event.preventDefault();
+//   const sentence = $("#vowel").val();
+//   const transformedSentence = vowelFinder(sentence);
+//   $("#output").append(transformedSentence);
+// });
